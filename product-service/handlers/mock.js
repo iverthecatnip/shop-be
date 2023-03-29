22d7971af9c3de2f74bedc99ebc727ae041e19b9
atolstova <atolstova@edmunds.com>
2023-03-29 17:38:26 +0600

    add tests

---
+++ product-service/handlers/mock.js
@@ -0,0 +1,38 @@
+export const mockProducts = [
+    {
+        description: "Short Product Description1",
+        id: "1",
+        price: 24,
+        title: "Product One",
+    },
+    {
+        description: "Short Product Description7",
+        id: "2",
+        price: 15,
+        title: "Product Two",
+    },
+    {
+        description: "Short Product Description2",
+        id: "3",
+        price: 23,
+        title: "Product Three",
+    },
+    {
+        description: "Short Product Description4",
+        id: "4",
+        price: 15,
+        title: "Product Four",
+    },
+    {
+        description: "Short Product Descriptio1",
+        id: "5",
+        price: 23,
+        title: "Product Five",
+    },
+    {
+        description: "Short Product Description7",
+        id: "6",
+        price: 15,
+        title: "Product Six",
+    },
+]
