0f71d79c049578f30454358f602632d49efc89a1
atolstova <atolstova@edmunds.com>
2023-05-12 16:59:41 +0600

    task 7

---
+++ authorization-service/handlers/basicAuthorizer.js
@@ -0,0 +1,42 @@
+export const basicAuthorizer = async (event, context, callback) => {
+    console.log('Event')
+    console.log(event)
+
+    if(event['type'] !== 'TOKEN'){
+        callback('Unauthorized request');
+
+    }
+
+    try {
+        const token = event.authorizationToken;
+
+        const encodedCreds = token.split(' ')[1];
+        const buff = Buffer.from(encodedCreds, 'base64');
+        const [username, password] = buff.toString('utf-8').split(':');
+
+        console.log(`username: ${username}, password: ${password}`);
+
+        const storedUserPassword = process.env[username];
+        const effect = !storedUserPassword || storedUserPassword !== password ? 'Deny' : 'Allow';
+        const policy = generatePolicy(encodedCreds, event.methodArn, effect)
+
+        callback(null, policy);
+    }catch(e) {
+       callback(`Unauthorized: ${e.message}`)
+
+    }
+};
+
+const generatePolicy = (principalId, resource, effect) => ({
+    principalId,
+    policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+            {
+                Action: 'execute-api:Invoke',
+                Effect: effect,
+                Resource: resource
+            }
+        ]
+    }
+});
